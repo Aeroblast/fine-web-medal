5f205039bd800089ac14a243c662902ad0e5bf93
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2024-07-27 10:27:19 +0900

    Update minplane.frag.glsl

--- src/shaders/minplane.frag.glsl
+++ src/shaders/minplane.frag.glsl
@@ -4,5 +4,5 @@ uniform sampler2D uSampler;
 
 void main(void) {
     highp vec4 baseColor = texture2D(uSampler, vTextureCoord);
-    gl_FragColor = baseColor * (0.5 + 0.5 * amb);
+    gl_FragColor = vec4(baseColor.xyz * (0.5 + 0.5 * amb), 1.0);
 }
